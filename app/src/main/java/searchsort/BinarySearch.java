0e02381b3221779e6719895473b54c35eaec96c2
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-12-04 12:45:56 -0500

    BinarySearch

--- app/src/main/java/searchsort/BinarySearch.java
+++ app/src/main/java/searchsort/BinarySearch.java
@@ -1,13 +1,36 @@
 package searchsort;
 
+import org.checkerframework.checker.units.qual.t;
+
 public class BinarySearch extends Search {
     /**
      * Find the index of the target element in the sorted array arr using binary
      * search. Return -1 if target is not found in arr.
+     * 
+     * {1,2,3,4,5,6} t = 5
      */
     @Override
     public int find(int[] arr, int target) {
-        return 0;
+        int low = 0;
+        int high = arr.length - 1;
+        
+        if(arr[0] == target){
+            return 0;
+        }
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            int cur = arr[mid];
+
+            if (cur == target) {
+                return mid;
+            } else if (cur < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -16,7 +39,25 @@ public class BinarySearch extends Search {
      */
     @Override
     public int recursiveFind(int[] arr, int target) {
-        return 0;
+        if(arr[0] == target){
+            return 0;
+        }
+        return recursiveFindHelper(arr, target, 0, arr.length - 1);
     }
+        private int recursiveFindHelper(int[] arr, int target, int low, int high) {
+            if (low > high) {
+                return -1;
+            }
 
-}
+            int mid = (high + low) / 2;
+            int cur = arr[mid];
+
+            if (cur == target) {
+                return mid;
+            } else if (cur < target) {
+                return recursiveFindHelper(arr, target, mid + 1, high);
+            } else {
+                return recursiveFindHelper(arr, target, low, mid - 1);
+            }
+        }
+    }
